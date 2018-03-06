66ede3281b52763445a0c24ed8f9e0143f2409a3
Max <36428120+AlfonsoHernandez@users.noreply.github.com>
2018-03-05 19:54:40 -0800

    Reorganized react components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './header';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Header />, document.getElementById('head'));
+ReactDOM.render(<App />, document.getElementById('head'));
 registerServiceWorker();

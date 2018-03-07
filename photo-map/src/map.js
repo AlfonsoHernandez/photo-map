5603537753d64723566c936327393971903d9f54
Max <alfonsomaxhernandez@gmail.com>
2018-03-07 00:22:23 -0800

    Added markers to map

--- photo-map/src/map.js
+++ photo-map/src/map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './Map.css';
+import Marker from './Marker'
 
 class Map extends Component {
     static defaultProps = {
@@ -12,9 +13,15 @@ class Map extends Component {
         return (
             <div className="map-container">
                 <GoogleMapReact
+                id="map"
                 defaultCenter={this.props.center}
                 defaultZoom={this.props.zoom}
-            />
+            >
+                <Marker
+                    lat={37.765883}
+                    lng={-122.478245}
+                />
+            </GoogleMapReact>
             </div>
         );
     }

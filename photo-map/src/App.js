1bf63a58af07e82fe23f23a3a61a7506c5478ba2
Alfonso <alfonsomaxhernandez@gmail.com>
2018-03-06 16:17:17 -0800

    Added comment

--- photo-map/src/App.js
+++ photo-map/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from './header';
-
+/**
+ *  Main component that renders all 
+ *  components
+ */
 class App extends Component {
   render() {
     return (

212a580b60f868f0333e6693e5ac793535331040
Alfonso <alfonsomaxhernandez@gmail.com>
2018-03-06 16:17:39 -0800

    Added comment

--- photo-map/src/index.js
+++ photo-map/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+/**
+ *  Main file that will take care of rendering
+ *  everything on the top level. 
+ */
 
 ReactDOM.render(<App />, document.getElementById('head'));
 registerServiceWorker();

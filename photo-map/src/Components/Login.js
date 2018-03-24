93339e27b9c51c529dee1f3fff2e4fb1769a3134
Alfonso <alfonsomaxhernandez@gmail.com>
2018-03-24 10:40:33 -0700

    Added closing form tag

--- photo-map/src/Components/Login.js
+++ photo-map/src/Components/Login.js
@@ -31,7 +31,7 @@ class Login extends Component{
         <div className='popup_inner'>
           <img onClick={this.props.closePopup} src="Icons/exit.png"/>
           <div className="body-title">Log in to continue</div>
-          <form onSubmit={this.onSubmit}
+          <form onSubmit={this.onSubmit}>
 
             <input
               placeholder="Email Adress"
@@ -48,7 +48,7 @@ class Login extends Component{
               Log in
             </button>
             <br/><br/>
-
+          </form>
           <a className="forgot" href="#">Forgot password?</a><br/><br/>
           <button id="oauth">Google<img id="google" src="Icons/google"/></button>
           <button id="oauth">Facebook<img id="fb" src="Icons/facebook"/></button>

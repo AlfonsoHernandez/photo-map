131cb1e2dc2690925c7d6f9dbdfb53376379ce50
Max <alfmaxhdz@gmail.com>
2018-04-17 00:26:38 -0700

    Redux boilerplate

--- photo-map/src/index.js
+++ photo-map/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 /**
  *  Main file that will take care of rendering
  *  everything on the top level.
  */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
